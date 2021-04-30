afff5bb3d17849732c3b31087e8487916ea4b09a
TrietMHo <tmh0999@gmail.com>
2021-04-29 20:42:15 -0700

    Started implementing minimal AI

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -19,7 +19,7 @@ from Action import Action
 class MyAI( AI ):
 
 	def __init__(self, rowDimension, colDimension, totalMines, startX, startY):
-
+	#
 		self.__rowDimension = rowDimension
 		self.__colDimension = colDimension
 		self.__totalMines = totalMines
@@ -28,16 +28,44 @@ class MyAI( AI ):
 
 		self.__currentX = startX
 		self.__currentY = startY
+		self.__currentMine = totalMines
 
-		self.
+		self.__board = dict()
+		self.__board[startX, startY] = 0
 
-		
+		self.__visited = {(x, y): False for y in range(1, colDimension+1) for x in range(1, rowDimension+1)}
+		self.__visited[(startX, startY)] = True;
+	#
+
+	@staticmethod
+	def __inBound(x: int, high: int, low: int = 0):
+	#
+		return low < x < high + 1
+	#
+
+	def getAdjacent(self, x: int, y: int) -> (int, int):
+	#
+		for i in (-1, 0, 1):
+			for j in (-1, 0, 1):
+				if (x,y) != (x+i, y+j) \
+					and self.__inBound(x+i, self.__rowDimension) \
+					and self.__inBound(y+j, self.__colDimension):
+					yield x+i, y+j
+	#
+
+	def processNode(self):
+	#
+		x = self.__currentX
+		y = self.__currentY
+		for (row, col) in tuple(self.getAdjacent(x, y)):
+
+		return None
+	#
+
 	def getAction(self, number: int) -> "Action Object":
+	#
 
-		########################################################################
-		#							YOUR CODE BEGINS						   #
-		########################################################################
-		return Action(AI.Action.LEAVE)
-		########################################################################
-		#							YOUR CODE ENDS							   #
-		########################################################################
+
+		return None
+	#
+
